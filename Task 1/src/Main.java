5c916fbdfc5401403c4175869b7852dc8832522b
Marlen Mels <marlenmels9@gmail.com>
2024-08-07 11:47:44 +0500

    Calculator have done

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,27 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        String answer = (b==0) ? "Деллитель не может быть равен нулю" : String.valueOf(a/b);
+        System.out.println(answer);
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        System.out.println(a-b);
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        System.out.println(a*b);
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        System.out.println(a+b);
     }
 
     private static double inputDouble() {
-        //todo дописать логику
-        return 0;
+        double input = new Scanner(System.in).nextDouble();
+        return input;
     }
 }
