0f2ace45e69e5e4043eeb7fcfa5c24130fe0b836
Marlen Mels <marlenmels9@gmail.com>
2024-08-07 12:07:18 +0500

    the task is done

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        check();
     }
+    public static void check(){
+        int input = new Scanner(System.in).nextInt();
+        System.out.println(input % 2 == 0);
+    }
+
 }
